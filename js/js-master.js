b1e6705425472cebac0672378102a5ee7f5465ee
Abhisek987 <123456abhisek@gmail.com>
2024-06-15 08:27:40 +0530

    Update js-master.js

--- js/js-master.js
+++ js/js-master.js
@@ -2,13 +2,13 @@ let count=0
 let cards=document.querySelectorAll(".content")
 function toggleTheme(){
     count++;
-    if(count % 2 !==0){
+    if(count % 2 != 0){
         document.querySelector("body").classList.remove("bg-black","text-white");
         document.querySelector("body").classList.add("bg-light","text-black");
         for(let i=0;i<cards.length;i++){
             let card = cards[i]
-            card.classList.remove("bg-dark","text-light");
-            card.classList.add("bg-white","text-dark");
+            card.classList.remove("bg-dark","text-white");
+            card.classList.add("bg-white","text-black");
         }
         document.querySelector('#acaTable').classList.remove("border-white","table-dark");
     }else{
@@ -16,10 +16,10 @@ function toggleTheme(){
         document.querySelector("body").classList.add("bg-black","text-white");
         for(let i=0;i<cards.length;i++){
             let card = cards[i]
-            card.classList.remove("bg-white","text-black");
+            card.classList.remove("bg-light","text-black");
             card.classList.add("bg-dark","text-white");
         }
-        document.querySelector('#acaTable').classList.remove("border-white","table-dark");
+        document.querySelector('#acaTable').classList.remove("border-black","table-white");
     }
 
 }
